3546713a2f69f8a344c1593dfba72dd031c9ec0f
Zhao Tang <zhao.tang@rice.edu>
2021-02-07 09:54:33 +0000

    FullyFunctionToPubV0

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "laiproject-instance.cteisoiuqd0e.us-east-2.rds.amazonaws.com";
+    private static final String INSTANCE = YOUR_INSTANCE_ADDRESS;
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "laiproject";
+    public static final String DB_NAME = YOUR_DB_NAME;
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "zt5lovesAWS!";
+    private static final String PASSWORD = YOUR_PASSWORD;
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
